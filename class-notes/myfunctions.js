289109b1fc5faed7e6ad0f89a8a864a4d9944477
rmendezbcn <rmendezvasquez@gmail.com>
2022-03-22 09:33:19 +0100

    TOC js fixed

--- class-notes/myfunctions.js
+++ class-notes/myfunctions.js
@@ -1,16 +1,17 @@
-let toc = document.getElementsByClassName('toc');
-const lenToc = toc.length;
-console.log(toc);
-console.log(lenToc);
+// collecting the elements that wil showup in the TOC
+let toc = document.getElementsByClassName("toc");
+let tocLen = toc.length;
+console.log(tocLen);
 let ul = document.createElement('ul');
-let li = document.createElement('li');
 
-for(let i = 0; i < lenToc; i++) {
-    console.log(i);
-    //console.log(toc.item(i).innerHTML);
+for (let i = 0; i < tocLen; i++) {
+    let li = document.createElement('li');
+    li.innerHTML = toc[i].innerHTML;
+    console.log(li.innerHTML);
+    ul.appendChild(li);
 }
+console.log(ul);
 
-//console.log(ul)
+let mytoc = document.getElementById('mytoc');
+mytoc.appendChild(ul);
 
-//let mytoc = document.getElementById('mytoc');
-//mytoc.appendChild(ul);
